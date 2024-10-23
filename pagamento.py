34d3fdb97e5760f9a81dee3de8fd8cb9502f36b1
lucasvilaca27 <lucas.vilaca@forteplus.com.br>
2024-10-23 10:15:03 -0300

    ajuste texto

--- pagamento.py
+++ pagamento.py
@@ -43,7 +43,7 @@ def webhook():
 
     # Enviar e-mail
     try:
-        msg = Message('Olá '+{nome_comprador}+', aqui estão seus dados do Forteplus',
+        msg = Message(f'Olá {nome_comprador}, aqui estão seus dados do Forteplus',
                       recipients=[email_comprador])
         msg.html = f"""
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
