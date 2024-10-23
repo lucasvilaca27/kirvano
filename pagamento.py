74505feed3f3d2aec885c82f24eb35ec3061d905
lucasvilaca27 <lucas.vilaca@forteplus.com.br>
2024-10-23 08:40:04 -0300

    ajuste layout email html

--- pagamento.py
+++ pagamento.py
@@ -45,7 +45,236 @@ def webhook():
     try:
         msg = Message('Obrigado pela sua compra!',
                       recipients=[email_comprador])
-        msg.body = f'Olá {nome_comprador},\n\nObrigado pela sua compra! Seus detalhes foram processados com sucesso. \nSeus dados de acesso: \nemail: {email_comprador}\nsenha: {senha}'
+        msg.html = f"""
+        <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="x-apple-disable-message-reformatting">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <title></title>
+          <style type="text/css">
+            @media only screen and (min-width: 520px) {{
+              .u-row {{
+                width: 500px !important;
+              }}
+              .u-row .u-col {{
+                vertical-align: top;
+              }}
+              .u-row .u-col-33p33 {{
+                width: 166.65px !important;
+              }}
+              .u-row .u-col-50 {{
+                width: 250px !important;
+              }}
+              .u-row .u-col-100 {{
+                width: 500px !important;
+              }}
+            }}
+            @media only screen and (max-width: 520px) {{
+              .u-row-container {{
+                max-width: 100% !important;
+                padding-left: 0px !important;
+                padding-right: 0px !important;
+              }}
+              .u-row {{
+                width: 100% !important;
+              }}
+              .u-row .u-col {{
+                display: block !important;
+                width: 100% !important;
+                min-width: 320px !important;
+                max-width: 100% !important;
+              }}
+              .u-row .u-col > div {{
+                margin: 0 auto;
+              }}
+              .u-row .u-col img {{
+                max-width: 100% !important;
+              }}
+            }}
+            body{{margin:0;padding:0}}
+            table,td,tr{{border-collapse:collapse;vertical-align:top}}
+            p{{margin:0}}
+            .ie-container table,.mso-container table{{table-layout:fixed}}
+            *{{line-height:inherit}}
+            a[x-apple-data-detectors=true]{{color:inherit!important;text-decoration:none!important}}
+            table, td {{ color: #000000; }} #u_body a {{ color: #0000ee; text-decoration: underline; }}
+          </style>
+        </head>
+        <body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #F7F8F9;color: #000000">
+          <table id="u_body" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #F7F8F9;width:100%" cellpadding="0" cellspacing="0">
+            <tbody>
+              <tr style="vertical-align: top">
+                <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
+                  <div class="u-row-container" style="padding: 0px;background-color: transparent">
+                    <div class="u-row" style="margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+                      <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
+                        <div class="u-col u-col-100" style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
+                          <div style="height: 100%;width: 100% !important;border-radius: 0px;">
+                            <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                              <tbody>
+                                <tr>
+                                  <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                                      <tr>
+                                        <td style="padding-right: 0px;padding-left: 0px;" align="center">
+                                          <img align="center" border="0" src="images/image-1.png" alt="" title="" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 20%;max-width: 96px;" width="96"/>
+                                        </td>
+                                      </tr>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                              <tbody>
+                                <tr>
+                                  <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                    <h1 style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-size: 25px; font-weight: 700;">
+                                      <span>Obrigado pela compra!</span>
+                                    </h1>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                              <tbody>
+                                <tr>
+                                  <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                    <div style="font-size: 14px; line-height: 140%; text-align: left; word-wrap: break-word;">
+                                      <p style="line-height: 140%; text-align: center;">🚀 Um pequeno clique para você mas um grande passo para seu negócio!</p>
+                                    </div>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                              <tbody>
+                                <tr>
+                                  <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                    <table height="0px" align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;border-top: 1px solid #BBBBBB;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%">
+                                      <tbody>
+                                        <tr style="vertical-align: top">
+                                          <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;font-size: 0px;line-height: 0px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%">
+                                            <span>&#160;</span>
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="u-row-container" style="padding: 0px;background-color: transparent">
+                      <div class="u-row" style="margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+                        <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
+                          <div class="u-col u-col-50" style="max-width: 320px;min-width: 250px;display: table-cell;vertical-align: top;">
+                            <div style="height: 100%;width: 100% !important;border-radius: 0px;">
+                              <div style="box-sizing: border-box; height: 100%; padding: 0px;">
+                                <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                  <tbody>
+                                    <tr>
+                                      <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                        <div style="font-size: 14px; line-height: 140%; text-align: left; word-wrap: break-word;">
+                                          <p style="line-height: 140%;">Suas credenciais de acesso ao <strong>Sistema Forteplus</strong>:</p>
+                                          <ul>
+                                            <li style="line-height: 19.6px;">Email: {email_comprador}</li>
+                                            <li style="line-height: 19.6px;">Senha: {senha}</li>
+                                          </ul>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                                <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                  <tbody>
+                                    <tr>
+                                      <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                        <div align="left">
+                                          <a href="" target="_blank" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #0068a5; border-radius: 4px;width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
+                                            <span style="display:block;padding:10px 20px;line-height:120%;"><span style="font-size: 14px; line-height: 16.8px;">Acessar Forteplus</span></span>
+                                          </a>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="u-col u-col-50" style="max-width: 320px;min-width: 250px;display: table-cell;vertical-align: top;">
+                            <div style="height: 100%;width: 100% !important;border-radius: 0px;">
+                              <div style="box-sizing: border-box; height: 100%; padding: 0px;">
+                                <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                  <tbody>
+                                    <tr>
+                                      <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                        <div style="font-size: 14px; line-height: 140%; text-align: left; word-wrap: break-word;">
+                                          <p style="line-height: 140%;">Suas credenciais para acesso ao mini curso na <strong>Kirvano:</strong></p>
+                                          <ul>
+                                            <li style="line-height: 19.6px;">Email: {email_comprador}</li>
+                                            <li style="line-height: 19.6px;">Senha: {senha}</li>
+                                          </ul>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                                <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                  <tbody>
+                                    <tr>
+                                      <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                        <div align="left">
+                                          <a href="" target="_blank" class="v-button" style="box-sizing: border-box;display: inline-block;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #161616; border-radius: 4px;width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
+                                            <span style="display:block;padding:10px 20px;line-height:120%;"><span style="font-size: 14px; line-height: 16.8px;">Acessar Kirvano</span></span>
+                                          </a>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="u-row-container" style="padding: 0px;background-color: transparent">
+                      <div class="u-row" style="margin: 0 auto;min-width: 320px;max-width: 500px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
+                        <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
+                          <div class="u-col u-col-100" style="max-width: 320px;min-width: 500px;display: table-cell;vertical-align: top;">
+                            <div style="height: 100%;width: 100% !important;border-radius: 0px;">
+                              <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                <tbody>
+                                  <tr>
+                                    <td style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
+                                      <div style="font-size: 14px; line-height: 140%; text-align: left; word-wrap: break-word;">
+                                        <p style="line-height: 140%;"><span style="font-size: 12px; line-height: 16.8px;">Dúvidas entre em contato através do suporte por telefone:<strong> (31) 3582-1410</strong></span></p>
+                                        <p style="line-height: 140%;"><span style="font-size: 12px; line-height: 16.8px;">© 2024 Forteplus Sistemas - Todos os direitos reservados.</span></p>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>
+        </html>
+        """
         mail.send(msg)
         logging.info(f'E-mail enviado para: {email_comprador}')  # Log de sucesso
     except Exception as e:
